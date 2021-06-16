f95b8df23d2018a4bda92d68222ac3b4baca95c5
Albertolopezmartin <alberniwe@hotmail.es>
2021-06-16 22:10:28 +0200

    construcción del login en el frontend

---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logout">
+      <form action class="form" @submit.prevent="logout">
+      <input class="form-submit" type="submit" value="¿Estás seguro que quieres cerrar sesión?">
+      </form>
+  </div>
+</template>
+
+<script>
+import auth from "@/logic/auth";
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    error: false
+  }),
+  methods: {
+    async logout() {
+        auth.deleteUserLogged();
+        this.$router.push("/");
+    }
+  }
+};
+</script>
