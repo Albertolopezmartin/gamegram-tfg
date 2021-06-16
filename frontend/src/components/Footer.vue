647bea9a5a546e680424f978bbbcecfa879e76b8
Albertolopezmartin <alberniwe@hotmail.es>
2021-06-16 19:40:35 +0200

    trabajo en el frontend

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer id="footer">
+    <nav id="menuf">
+      <ul>
+        <li>
+          <router-link to="/home" active-class="active">Inicio</router-link>
+        </li>
+        <li>
+          <router-link to="/games" active-class="active">Juegos</router-link>
+        </li>
+        <li>
+          <router-link to="/photos" active-class="active">Fotos</router-link>
+        </li>
+        <li>
+          <router-link to="/perfil" active-class="active">Perfil</router-link>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header id="header">
+        <div class="center">
+        <!-- LOGO -->
+            <div id="logo">
+                <img src="../assets/logo.png" class="app-logo" alt="Logotipo">
+                <span id="brand">
+                    <strong>GAMEGRAM</strong>
+                </span>
+            </div>
+        <!-- MENU -->
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <router-link to="/home" active-class="active">Inicio</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/games" active-class="active">Juegos</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/photos" active-class="active">Fotos</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/perfil" active-class="active">Perfil</router-link>
+                    </li>
+                </ul>
+            </nav>
+
+            <!--LIMPIAR FLOATADOS-->
+            <div class="clearfix"></div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
